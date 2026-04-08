9d217ad8a7ef8bbec4a8d4362d667cdf476c3dd6
Marcos Araújo <mvbdearaujo@gmail.com>
2026-04-08 05:16:48 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,25 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+  char estado1 = Pernambuco;
+  char codigodacarta1[20] = "R01", "R02", "R03" e "R04";
+  char nomedacidade1[20] = Recife;
+  int populacao1 = 1.490.000;
+  float areaemkm21 = 218.00;
+  float pib1 = 54.900.000.000.00;
+  int numerodepontosturisticos1 = 4;
 
+  // Área para entrada de dados
+  
+  printf("Estado: %s\n", estado1);
+  printf("Estado: %s\n", codigodacarta1);
+  printf("Estado: %s\n", nomedacidade1);
+  printf("Estado: %d \n", populacao1);
+  printf("Estado: %.2f \n", areaemkm21);
+  printf("Estado: %.2f \n", pib1);
+  printf("Estado: %d \n", numerodepontosturisticos1);
+
+  // Inacabado.
   // Área para exibição dos dados da cidade
 
 return 0;
